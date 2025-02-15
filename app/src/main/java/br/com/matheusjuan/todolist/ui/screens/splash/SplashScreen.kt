d6b8f3634e7d136a45d7abdea63f3c078ab60d33
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-02-15 00:06:54 -0300

    FEAT[Splash] Adiciona tela Splash com imagens provisórias

--- app/src/main/java/br/com/matheusjuan/todolist/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/br/com/matheusjuan/todolist/ui/screens/splash/SplashScreen.kt
@@ -1,2 +1,44 @@
 package br.com.matheusjuan.todolist.ui.screens.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.matheusjuan.todolist.R
+import br.com.matheusjuan.todolist.ui.theme.BlueBase
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(BlueBase)
+            .fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.ic_circle_check),
+            contentDescription = stringResource(id = R.string.app_logo)
+        )
+        Image(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 40.dp),
+            painter = painterResource(id = R.drawable.ic_fingerprint),
+            contentDescription = ""
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
