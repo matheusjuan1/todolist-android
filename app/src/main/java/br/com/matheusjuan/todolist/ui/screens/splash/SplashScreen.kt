67282a352359196c37115d62b5ac541d589893ff
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-02-17 14:35:46 -0300

    FEAT[SplashScreen] Adiciona novos recursos e altera layout SplashScreen

--- app/src/main/java/br/com/matheusjuan/todolist/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/br/com/matheusjuan/todolist/ui/screens/splash/SplashScreen.kt
@@ -1,13 +1,15 @@
 package br.com.matheusjuan.todolist.ui.screens.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,17 +24,22 @@ fun SplashScreen(modifier: Modifier = Modifier) {
             .background(BlueBase)
             .fillMaxSize()
     ) {
-        Image(
-            modifier = Modifier.align(Alignment.Center),
+        Icon(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(90.dp),
             painter = painterResource(id = R.drawable.ic_circle_check),
-            contentDescription = stringResource(id = R.string.app_logo)
+            contentDescription = stringResource(id = R.string.app_logo),
+            tint = Color.White
         )
-        Image(
+        Icon(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = 40.dp),
-            painter = painterResource(id = R.drawable.ic_fingerprint),
-            contentDescription = ""
+                .padding(bottom = 50.dp)
+                .size(54.dp),
+            painter = painterResource(id = R.drawable.logo_mj),
+            tint = Color.White,
+            contentDescription = stringResource(id = R.string.logo_mj)
         )
     }
 }
