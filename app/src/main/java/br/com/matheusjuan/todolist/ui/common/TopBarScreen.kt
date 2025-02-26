a2a89c362f91c9f3f9e9a60fdac7e8bf15f210d2
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-02-26 00:40:47 -0300

    feat: Adiciona fab a tela home

--- app/src/main/java/br/com/matheusjuan/todolist/ui/common/TopBarScreen.kt
+++ app/src/main/java/br/com/matheusjuan/todolist/ui/common/TopBarScreen.kt
@@ -3,15 +3,19 @@ package br.com.matheusjuan.todolist.ui.common
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import br.com.matheusjuan.todolist.ui.components.button.TodoFloatingActionButton
 import br.com.matheusjuan.todolist.ui.components.topbar.TodoTopBar
 
 @Composable
 fun TopBarScreen(
-    content: @Composable (PaddingValues) -> Unit
+    content: @Composable (PaddingValues) -> Unit,
 ) {
     Scaffold(
         topBar = {
-            TodoTopBar {  }
+            TodoTopBar { }
+        },
+        floatingActionButton = {
+            TodoFloatingActionButton { }
         }
     ) { paddingValues ->
         content(paddingValues)
