952a18e9d253ee79e4405787e4548977a2270615
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-03-06 16:39:55 -0300

    feat: title for screens

--- app/src/main/java/br/com/matheusjuan/todolist/ui/screens/task/TaskEditScreen.kt
+++ app/src/main/java/br/com/matheusjuan/todolist/ui/screens/task/TaskEditScreen.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.matheusjuan.todolist.R
 import br.com.matheusjuan.todolist.ui.components.button.TodoButton
+import br.com.matheusjuan.todolist.ui.theme.Typography
 
 @Composable
 fun TaskEditScreen(
@@ -30,10 +35,17 @@ fun TaskEditScreen(
             modifier = Modifier
                 .padding(start = 12.dp, end = 12.dp, top = 16.dp, bottom = 50.dp)
         ) {
-            TodoButton(
-                iconRes = R.drawable.ic_arrow_left,
-                onClick = onNavigateBack
-            )
+            Box(modifier = Modifier.fillMaxWidth()) {
+                TodoButton(
+                    iconRes = R.drawable.ic_arrow_left,
+                    onClick = onNavigateBack
+                )
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = stringResource(R.string.edit),
+                    style = Typography.headlineLarge
+                )
+            }
         }
     }
 }
